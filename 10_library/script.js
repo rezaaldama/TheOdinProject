5c37136af7eaf4f80aa2dffadcbc56cc0044db09
raaldama <aldamareza@gmail.com>
2026-01-26 05:59:31 +0100

    Initial commit for 10_library

---
+++ 10_library/script.js
@@ -0,0 +1,136 @@
+const myLibrary = new Array();
+
+function Book(title, author, pages, status) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+// Book.prototype.info = function () {
+//     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`
+// }
+
+const bookContainer = document.querySelector('#book-container');
+
+bookContainer.addEventListener('click', (e) => {
+    const bookCard = e.target.closest('.book');
+    if (!bookCard) return;
+
+    const index = bookCard.getAttribute('data-index');
+
+    if (e.target.closest('.remove-btn')) {
+        myLibrary.splice(index, 1);
+        displayBooks();
+        return;
+    }
+
+    if (e.target.closest('.status-btn')) {
+        myLibrary[index].isRead = !myLibrary[index].isRead;
+        displayBooks();
+    }
+});
+
+const openBtn = document.querySelector("#openDialogBtn");
+const dialog = document.querySelector("#bookDialog");
+
+openBtn.addEventListener("click", () => {
+    dialog.showModal();
+
+    const modalWidth = dialog.offsetWidth;
+    const modalHeight = dialog.offsetHeight;
+
+    const btnRect = openBtn.getBoundingClientRect();
+
+    const topPos = btnRect.top - modalHeight - 10;
+    const leftPos = btnRect.left - modalWidth + 20;
+
+    dialog.style.top = `${topPos}px`;
+    dialog.style.left = `${leftPos}px`;
+});
+
+const bookForm = document.querySelector('#bookForm');
+
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isRead = document.querySelector('#isRead').checked;
+
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+
+    displayBooks();
+
+    bookForm.reset();
+    dialog.close();
+});
+
+function displayBooks() {
+    bookContainer.innerHTML = "";
+
+    myLibrary.forEach((book, index) => {
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+        bookDiv.setAttribute('data-index', index);
+
+        bookDiv.innerHTML = `
+            <div class="book-info">
+                <div class="title">${book.title}</div>
+                <div class="author">by ${book.author}</div>
+            </div>
+            <div class="book-meta">
+                <div class="pages">${book.pages} pages</div>
+                <button class="status-btn ${book.isRead ? 'read' : 'not-read'}">
+                    ${book.isRead ? 'Read' : 'Not Read'}
+                </button>
+                <button class="remove-btn">
+                    <span class="material-icons">delete_outline</span>
+                </button>
+            </div>
+        `;
+        bookContainer.appendChild(bookDiv);
+    });
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+
+    bookDiv.innerHTML = `
+    <div class="book-info">
+        <div class="title">${title}</div>
+        <div class="author">by ${author}</div>
+    </div>
+    <div class="book-meta">
+        <div class="pages">${pages} pages</div>
+        <button class="status-btn ${isRead ? 'read' : 'not-read'}">
+            ${isRead ? 'Read' : 'Not Read'}
+        </button>
+        <button class="remove-btn">
+            <span class="material-icons">delete_outline</span>
+        </button>
+    </div>
+    `;
+
+    bookContainer.appendChild(bookDiv);
+
+    bookForm.reset();
+    dialog.close();
+}
+
+const closeBtn = document.querySelector('#cancelBtn');
+closeBtn.addEventListener('click', () => {
+    bookForm.reset();
+    dialog.close();
+});
+
+// Testing
+const orwellBook = new Book('1984', 'George Orwell', '268', 'Read');
+myLibrary.push(orwellBook);
+displayBooks();
